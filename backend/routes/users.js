9df355e324ee92e90caee37397de186993119f37
tiennguyenvan <nguyentien.jobs@gmail.com>
2024-10-30 16:38:22 -0700

    Add get-all for user rout

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -198,4 +198,16 @@ router.get('/:userId/following', async (req, res) => {
     }
 });
 
+router.get('/get-all', async (req, res) => {
+    try {
+        const users = await User.find(); // Fetch all users
+        console.log('All Users:', users); // Print users to the console
+        res.status(200).json(users); // Send users as response to confirm
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Error retrieving users.' });
+    }
+});
+
+
 module.exports = router;
