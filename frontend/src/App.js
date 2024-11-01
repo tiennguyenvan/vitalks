760f438b00a358ca93abd0cba4a5b383e89f99e8
YaolongLiu <Yaolong3752@gmail.com>
2024-10-31 18:12:02 -0700

    FEAT: update profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import HomePage from './pages/home/HomePage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
                     {/* Add more routes here as needed 
 					ex: <Route path="/" element={<HomePage />} />
 					ex: <Route path="/profile" element={<ProfilePage />} />
