7e2c6a0b69af1e6673a0b119d40a3c7656e80e00
Diego <diegopinlac@gmail.com>
2024-11-03 08:49:56 -0800

    FEAT: Add Login Responsiveness

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
 	return (<>
 		<div className="page page-login">
 			<div className="row">
-				<div className="col-md-7">
+				<div className="col-md-7 col-sm-12">
 					<div className="hero">
 						<div className="hero__body centered">
 							<h2>Strengthening Mental Health Support to Empower Thriving Communities with <span className="yellow">ViTalks</span></h2>
@@ -17,10 +17,10 @@ const LoginPage = () => {
 						<img alt='Login Divider' className="divider" src={loginDivider} />
 					</div>
 				</div>
-				<div className="col-md-5">
+				<div className="col-md-5 col-sm-12">
 					<div className="form">
 						<SiteLogo />
-						<LoginForm title={<>Welcome<br />Back</>} />
+						<LoginForm title={<>Welcome Back</>} />
 					</div>
 				</div>
 			</div>
