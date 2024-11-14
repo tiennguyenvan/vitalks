46fdd3f674e253c33f91f129e3ad58e794336476
Diego <diegopinlac@gmail.com>
2024-11-13 20:42:19 -0800

    FEAT: Add active menu based on current page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../App.css';
 import userPhoto from '../assets/images/profile-user-photo-1.png';
 import { FaBars, FaHouse, FaCircleUser, FaMagnifyingGlass, FaAngleDown } from "react-icons/fa6";
 import SiteLogo from './SiteLogo';
+import { useLocation }  from 'react-router-dom';
 
 const Header = () => {
 	const [inSearch, setSearchValue] = useState('');
@@ -21,7 +22,10 @@ const Header = () => {
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
-	
+
+	const location = useLocation().pathname.split('/');
+
+	console.log(location[1]);
 
 	return (
 		<>
@@ -41,16 +45,18 @@ const Header = () => {
 						</form>
 					</div>
 
+					
+
 					<div className="navbar__right">
 						<span className="hamburger" onClick={toggleMenu}><FaBars /></span>
 
 						<div className={`navbar__links ${isMenuOpen ? 'active' : ''}`}>
 							<div className="navbar__link__container">
-								<a href="/" className="navbar__link navbar__link--active">
+								<a href="/" className={`navbar__link ${location[1] ? '' : 'navbar__link--active'}`}>
 									<span className="nav__icon"><FaHouse /></span>
 									<span className="nav__label">Home</span>
 								</a>
-								<a href={`/profile/${(currentUser && currentUser._id) ? currentUser._id: ''}`} className="navbar__link">
+								<a href={`/profile/${(currentUser && currentUser._id) ? currentUser._id: ''}`} className={`navbar__link ${location[1] == 'profile' ? 'navbar__link--active' : ''}`}>
 									<span className="nav__icon"><FaCircleUser /></span>
 									<span className="nav__label">Profile</span>
 								</a>
