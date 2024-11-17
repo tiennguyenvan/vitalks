ba4881fe490333f4d3810471fd9dacd393160957
Diego <diegopinlac@gmail.com>
2024-11-17 00:02:38 -0800

    FEAT: Removed unnecessary pages/files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import HomePage from './pages/home/HomePage';
-import CommunityPage from './pages/community/CommunityPage';
-import MessagingPage from './pages/messaging/Messaging';
-import NotificationPage from './pages/notification/NotificationPage';
 import ProfilePage from './pages/profile/ProfilePage';
-import ErrorPage from './pages/error/ErrorPage';
+import ErrorPage from './pages/status/UnderConstructionPage';
 import { isUserLoggedIn } from './utils/Lib';
 function App() {	
 	return (
@@ -16,9 +13,6 @@ function App() {
 					<Route path="/login" element={<LoginPage />} />
 					{isUserLoggedIn() ? <>						
 						<Route path="/" element={<HomePage />} />
-						<Route path="/community" element={<CommunityPage />} />
-						<Route path="/messaging" element={<MessagingPage />} />
-						<Route path="/notification" element={<NotificationPage />} />
 						<Route path="/profile/:id" element={<ProfilePage />} />						
 						<Route path="*" element={<ErrorPage />} />
 					</> : 
